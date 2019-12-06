086ec07b848f1af19f26c8c3a55978f5758e72a6
Patrick Goulding <patrickgoulding@gmail.com>
2019-12-06 15:50:20 -0700

    Add footer component

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.scss'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          About Us
+        </li>
+        <li>
+          Contact
+        </li>
+        <li>
+          Legal
+        </li>
+        <li>
+          Careers
+        </li>
+      </ul>
+    </footer>
+  )
+}

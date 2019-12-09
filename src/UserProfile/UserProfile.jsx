37d2f9def24fd8bdb5180c1ef4b013507c1517be
Patrick Goulding <patrickgoulding@gmail.com>
2019-12-08 20:49:55 -0700

    add handle change to all inputs

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { TextField, Button } from '@material-ui/core'
 import { useStyles } from "../muiTheme";
-
+import './UserProfile.scss'
 export const UserProfile = (props) => {
     const classes = useStyles();
 
+    const handleTextChange = e => {
+      e.preventDefault()
+      props.handleChange(e)
+    }
+    
     const continueForm = e => {
       e.preventDefault();
       props.nextStep();
@@ -19,19 +24,64 @@ export const UserProfile = (props) => {
     <div>
       <TextField
         label="UserName"
-        defaultValue={props.values.userName}
+        defaultValue={props.values.username}
         InputProps={{
           readOnly: true
         }}
         fullWidth="true"
         type="text"
       />
-      <Button onClick={previousForm} className={classes.button}>
-        Previous
-      </Button>
-      <Button onClick={continueForm} className={classes.button}>
-        NEXT
-      </Button>
+      <TextField
+        label="First Name"
+        name="firstName"
+        fullWidth="true"
+        type="text"
+        onChange={handleTextChange}
+      />
+      <TextField
+        label="Last Name"
+        name="lastName"
+        fullWidth="true"
+        type="text"
+        onChange={handleTextChange}
+      />
+      <TextField
+        label="Street"
+        name="street"
+        fullWidth="true"
+        type="text"
+        onChange={handleTextChange}
+      />
+      <TextField
+        label="State"
+        name="addressState"
+        fullWidth="true"
+        type="text"
+        onChange={handleTextChange}
+      />
+      <TextField
+        label="Zip Code"
+        name="zipCode"
+        fullWidth="true"
+        type="text"
+        onChange={handleTextChange}
+      />
+      <TextField
+        label="Phone Number"
+        name="phoneNumber"
+        fullWidth="true"
+        type="tel"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        onChange={handleTextChange}
+      />
+      <div className="form_nav-buttons">
+        <Button onClick={previousForm} className={classes.button}>
+          Previous
+        </Button>
+        <Button onClick={continueForm} className={classes.button}>
+          NEXT
+        </Button>
+      </div>
     </div>
   );
 }

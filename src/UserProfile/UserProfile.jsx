46dad2487fceaaa673f9e3a1d56f7c3a87b62b84
Patrick Goulding <patrickgoulding@gmail.com>
2019-12-08 19:19:40 -0700

    Refactor to multistep form with MaterialUI

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -1,1 +1,39 @@
+import React from 'react'
+import { TextField, Button } from '@material-ui/core'
+import { useStyles } from "../muiTheme";
 
+export const UserProfile = (props) => {
+    const classes = useStyles();
+
+    const continueForm = e => {
+      e.preventDefault();
+      props.nextStep();
+    };
+
+    const previousForm = e => {
+        e.preventDefault();
+        props.previousStep();
+    }
+
+  return (
+    <div>
+      <TextField
+        label="UserName"
+        defaultValue={props.values.userName}
+        InputProps={{
+          readOnly: true
+        }}
+        fullWidth="true"
+        type="text"
+      />
+      <Button onClick={previousForm} className={classes.button}>
+        Previous
+      </Button>
+      <Button onClick={continueForm} className={classes.button}>
+        NEXT
+      </Button>
+    </div>
+  );
+}
+
+export default UserProfile
